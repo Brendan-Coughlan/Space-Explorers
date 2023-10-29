9ea91b969a53fed93b8444253cc69fe4fc5ea31d
Brendan Coughlan <bcoughlan2404@gmail.com>
2023-10-29 13:10:41 -0400

    Device Change

---
+++ Space Explorers/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShipMovement : MonoBehaviour
+{
+    Rigidbody2D shipRb;
+
+    private PlayerInput playerInput;
+    private InputAction movementAction;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        movementAction = playerInput.SwitchCurrentActionMap("Ship");
+        shipRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Keyboard.current.tKey.wasPressedThisFrame)
+        {
+            Debug.Log("T");
+            shipRb.AddForce(200 * Vector2.up * Time.deltaTime, ForceMode2D.Impulse);
+        }
+    }
+}
